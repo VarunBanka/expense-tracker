ccb08524295731c3d68c160048230a15e208c0e5
Varun Banka <bankavarun18@gmail.com>
2023-11-01 12:33:35 +0530

    added support for dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,29 @@ var kColorSchm = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColourSchm = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColourSchm,
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorSchm,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorSchm.onPrimaryContainer,
-          foregroundColor: kColorSchm.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorSchm.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
+        // appBarTheme: const AppBarTheme().copyWith(
+        // backgroundColor: kColorSchm.onPrimaryContainer,
+        // foregroundColor: kColorSchm.primaryContainer,
+        // ),
+        // cardTheme: const CardTheme().copyWith(
+        // color: kColorSchm.secondaryContainer,
+        // margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        // ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: kColorSchm.primaryContainer,
@@ -31,6 +40,7 @@ void main() {
               fontSize: 14,
             )),
       ),
+      themeMode: ThemeMode.system,
       home: const ExpensesScreen(),
     ),
   );
